c52cbbd974c033dfd308c0f8eee8c89640bc8ba4
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-11 03:57:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,10 +7,10 @@ end
 
 def run_guessing_game
   pc_num = (rand(6) + 1)
-  player_num = player_prompt.to_i
+  player_num = player_prompt
   if player_num == "exit" 
     puts "Goodbye!" 
-  elsif player_num == pc_num
+  elsif player_num.to_i == pc_num
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{pc_num}."
